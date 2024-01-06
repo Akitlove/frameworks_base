48ce73a21d98db2c840055159a9dc0ee1634fdf6
cjh1249131356 <cjh1249131356@gmail.com>
2024-01-06 21:48:51 +0800

    SystemUI: Implement RefreshRateManager [1/2]
    Includes:
    - Per-app refresh rate config
    - Extreme refresh rate (Force all apps to run in maximum refresh rate)
    - Temp refresh rate allowed (Used in MEMC)
    - Refresh rate QS tile
    
    Change-Id: I1a5d4ccaf18389a0ea26342bd682856faa3ab17a
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [nurkeinneid: move to fwb and drop sdk extensions/dependencies]
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5262,12 +5262,6 @@ public final class Settings {
         public static final String FOLD_LOCK_BEHAVIOR = "fold_lock_behavior_setting";
 
         /**
-         * Whether refresh rate should be switched to 60Hz on power save mode.
-         * @hide
-         */
-        public static final String LOW_POWER_REFRESH_RATE = "low_power_rr_switch";
-
-        /**
          * The amount of time in milliseconds before the device goes to sleep or begins
          * to dream after a period of inactivity.  This value is also known as the
          * user activity timeout period since the screen isn't necessarily turned off
@@ -7374,6 +7368,18 @@ public final class Settings {
          */
         @Readable
         public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
+
+        /**
+         * Per-app refresh rate config
+         * @hide
+         */
+        public static final String REFRESH_RATE_CONFIG_CUSTOM = "refresh_rate_config_custom";
+
+        /**
+         * Force highest refresh rate in all apps
+         * @hide
+         */
+        public static final String EXTREME_REFRESH_RATE = "extreme_refresh_rate";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
@@ -19654,6 +19660,12 @@ public final class Settings {
          * @hide
          */
         public static final String ONE_HANDED_KEYGUARD_SIDE = "one_handed_keyguard_side";
+
+        /**
+         * Whether refresh rate should be switched to 60Hz on power save mode.
+         * @hide
+         */
+        public static final String LOW_POWER_REFRESH_RATE = "low_power_rr_switch";
 
         /**
          * A list of uids that are allowed to use restricted networks.
